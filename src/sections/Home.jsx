7505275495753246ca857b54797f7ed2afbefaeb
Arkadip <arkadip671@gmail.com>
2025-01-29 15:01:30 +0530

    added status card

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -5,40 +5,31 @@ import { Element } from "react-scroll";
 
 const Home = () => {
   return (
-    <section className="container h-screen border-2 rounded-xl border-[#1E3E62] flex items-center justify-center bg-gradient-to-b from-[#1d1d1d] to-[#1E3E62] text-white">
-          <Element name="home">
+    <section>
+      <Element name="home">
+        <div className="container">
 
+        <div className="max-w-[1250px] bg-linear-to-b from-[#111] to-[#222] p-[20px] border-2 rounded-[10px] border-amber-400 text-white text-3xl">
+          <h2>Status</h2>
+        <p><strong>Name:</strong> Sung Jin-Woo <span className="float-right">LV.146</span></p>
+        <p><strong>Class:</strong> Shadow Monarch</p>
+        <p><strong>Title:</strong> Demon Hunter (2 Others)</p>
+        <hr />
+        <p><strong>HP:</strong> 93,300 <span className="float-right"><strong>MP:</strong> 155,720</span></p>
+        <p><strong>Fatigue:</strong> 0</p>
+        <hr />
+        <div className="flex flex-wrap justify-between">
+            <p><strong>Strength:</strong> 324</p>
+            <p><strong>Stamina:</strong> 320</p>
+            <p><strong>Agility:</strong> 340</p>
+            <p><strong>Intelligence:</strong> 340</p>
+            <p><strong>Perception:</strong> 321</p>
+            <p><strong>AP:</strong> 0</p>
+        </div>
+        </div>
 
-      <div className="text-center max-w-4xl px-4">
-        <h1
-          className="text-4xl md:text-6xl font-bold mb-4"
-          >
-          ARKADIP DAS
-        </h1>
-        {/* <motion.p
-          className="text-lg md:text-xl mb-6"
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.2 }}
-          >
-          A <span className="font-semibold">Web Developer</span> and <span className="font-semibold">AI/ML Enthusiast</span> building interactive digital experiences.
-          </motion.p> */}
-
-        {/* <motion.div
-          className="flex justify-center space-x-4"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8, delay: 0.4 }}
-          >
-          <Button className="bg-yellow-400 text-black hover:bg-yellow-300 px-6 py-3 rounded-xl shadow-lg font-semibold text-lg">
-          View My Projects
-          </Button>
-          <Button className="bg-white text-blue-600 hover:bg-gray-200 px-6 py-3 rounded-xl shadow-lg font-semibold text-lg">
-          Contact Me
-          </Button>
-          </motion.div> */}
-      </div>
-          </Element>
+        </div>
+      </Element>
     </section>
   );
 };
