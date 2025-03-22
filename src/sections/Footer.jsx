519ab388fb0127afc645afe7b12ebf06e16494f3
Arkadip <arkadip671@gmail.com>
2025-03-23 02:16:16 +0530

    changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,14 +3,12 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      
-        <div>
-          <ul className="flex flex-1 items-center px-20 justify-between mx-auto py-20 border-3 border-gray-900">
-            <li>Arkadip Das</li>
-            <li>All rights reserved.</li>
-            <li>Used Resources</li>
-          </ul>
-  
+      <div>
+        <ul className="flex flex-1 items-center px-20 justify-between mx-auto py-20 border-3 border-gray-900">
+          <li>Arkadip Das</li>
+          <li>All rights reserved.</li>
+          <li>Used Resources</li>
+        </ul>
       </div>
     </footer>
   );
