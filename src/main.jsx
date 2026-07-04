2a64b128a7fe645d7f16b0c89ddbae0141419634
Arkadip <arkadip671@gmail.com>
2026-07-04 17:44:47 +0530

    smooth scroll

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,19 @@ import App from './App.jsx'
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import Lenis from 'lenis';
+
+const lenis = new Lenis({
+  duration: 1.5,
+  smoothWheel: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
